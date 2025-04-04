d7242f7484e15b2e3adc7e08e75b2d2a9e266bf0
MilesFike <156404000+MilesFike@users.noreply.github.com>
2025-04-04 13:31:44 -0400

    Add files via upload

--- chips.py
+++ chips.py
@@ -2,7 +2,7 @@ class Chips:
 
 
 
-        def __init__(self, fiveCount, tenCount, twentyCount, oneCount):
+        def __init__(self, twentyCount, tenCount, fiveCount, oneCount):
                 self.fiveCount = fiveCount
                 self.tenCount = tenCount
                 self.twentyCount = twentyCount
@@ -12,7 +12,21 @@ class Chips:
                 total = (self.fiveCount * 5) + (self.tenCount * 10) + (self.twentyCount * 20) + (self.oneCount * 1) # type: ignore
                 if(total > 50000):
                         print("You are way too wealthy sonny boy")
+        
         def add(self, number):
+                twenties = int(number/20)
+                self.twentyCount = self.twentyCount + twenties
+                number = number - (twenties * 20) # subtract the value of twenties from number
+                tens = int(number/10)
+                self.tenCount = self.tenCount + tens
+                number = number - (tens * 10)
+                fives = int(number/5) 
+                self.fiveCount = self.fiveCount + fives
+                number = number - (fives * 5)
+                ones = int(number)
+                self.oneCount = self.oneCount + ones
+ 
+        def playChips(self, number):
                 twenties = int(number/20)
                 if(twenties <= self.twentyCount):
                         self.twentyCount = self.twentyCount - twenties
@@ -52,14 +66,38 @@ class Chips:
                 self.oneCount = 0
                 return total
         def printChips(self):
-                print(self.twentyCount, "twenties", self.tenCount, "tens", self.fiveCount, "fives", self.oneCount, "ones")       
-Player1 = Chips(1000, 20000, 22398, 9423)
-Player1.upperlimit()
-Player1.printChips()
-Player1.add(53)
-Player1.printChips()
-Player1.allIn()
-Player1.printChips()
+                print(self.twentyCount, "twenties", self.tenCount, "tens", self.fiveCount, "fives", self.oneCount, "ones")    
+        def printTotal(self):
+                total = (self.fiveCount * 5) + (self.tenCount * 10) + (self.twentyCount * 20) + (self.oneCount * 1)
+                print("Total Chips: ", total)
+                if(total <= 0):
+                        print("You lose")
+                        self.fiveCount = 0
+                        self.tenCount = 0
+                        self.twentyCount = 0
+                        self.oneCount = 0
+                return total
+        def giveTotal(self):
+                total = (self.fiveCount * 5) + (self.tenCount * 10) + (self.twentyCount * 20) + (self.oneCount * 1)
+                return total
+
+        
+if __name__ == "__main__":
+                          
+        Player1 = Chips(1000, 20000, 22398, 9423)
+        Player1.upperlimit()
+        Player1.printChips()
+        Player2 = Chips
+        Player1.add(53)
+        Player1.printChips()
+        Player1.playChips(10)
+        total = Player1.printTotal()
+        print(total)
+        Player1.printChips()
+        Player1.add(5392)
+        Player1.printChips()
+        Player1.allIn()
+        Player1.printChips()
 
 
         
--- logic.py
+++ logic.py
@@ -1,52 +1,62 @@
-# PokerGame.py
-from card import Card, makeStandardDeck, shuffleDeck, dealHand
+from card import makeStandardDeck, shuffleDeck, dealHand, deal
+from chips import Chips
+import random
+
+botBank = Chips(0, 0, 0, 0)
+playerBank = Chips(0, 0, 0, 0)
+opponentChips = input("How many chips do you want your opponent to have?: \n")
+playerChips = input("How many chips do you want to start with?: \n")
+botBank.add(int(opponentChips))
+playerBank.add(int(playerChips))
+botBank.printChips()
+botBank.printTotal()
+playerBank.printChips()
+playerBank.printTotal()
+
+
+def card_value(card):
+    face_card_values = {'Ace': 1, 'Jack': 11, 'Queen': 12, 'King': 13}
+    return face_card_values.get(card.value, int(card.value) if card.value.isdigit() else card.value)
 
 def is_flush(hand):
-    suits = [card.get_suit() for card in hand]
+    suits = [card.suit for card in hand]
     return len(set(suits)) == 1
 
-
 def is_straight(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     values.sort()
     return values == list(range(values[0], values[0] + 5))
-
 
 def is_straight_flush(hand):
     return is_flush(hand) and is_straight(hand)
 
-
 def is_royal_flush(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     return is_straight_flush(hand) and values[0] == 1 and values[-1] == 13
 
-
 def is_three_of_a_kind(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     for value in values:
         if values.count(value) == 3:
             return True
     return False
-    
 
 def is_four_of_a_kind(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     for value in values:
         if values.count(value) == 4:
             return True
     return False
 
-
 def is_five_of_a_kind(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     for value in values:
         if values.count(value) == 5:
             return True
     return False
 
-
 def is_full_house(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     for value in values:
         if values.count(value) == 3:
             for value in values:
@@ -54,18 +64,16 @@ def is_full_house(hand):
                     return True
     return False
 
-
 def is_two_pair(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     pairs = 0
     for value in values:
         if values.count(value) == 2:
             pairs += 1
     return pairs == 2
 
-
 def is_one_pair(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     pairs = 0
     for value in values:
         if values.count(value) == 2:
@@ -73,26 +81,139 @@ def is_one_pair(hand):
     return pairs == 1
 
 def is_high_card(hand):
-    values = [card.get_value() for card in hand]
+    values = [card_value(card) for card in hand]
     pairs = 0
     for value in values:
         if values.count(value) == 1:
             pairs += 1
     return pairs == 5
 
-# Create a deck, shuffle it, and deal a hand
-testDeck = makeStandardDeck()
-shuffleDeck(testDeck)
+def hand_value(hand):
+    if is_royal_flush(hand):
+        return (10, "Royal Flush")
+    elif is_straight_flush(hand):
+        return (9, "Straight Flush")
+    elif is_four_of_a_kind(hand):
+        return (8, "Four of a Kind")
+    elif is_full_house(hand):
+        return (7, "Full House")
+    elif is_flush(hand):
+        return (6, "Flush")
+    elif is_straight(hand):
+        return (5, "Straight")
+    elif is_three_of_a_kind(hand):
+        return (4, "Three of a Kind")
+    elif is_two_pair(hand):
+        return (3, "Two Pair")
+    elif is_one_pair(hand):
+        return (2, "One Pair")
+    else:
+        return (1, "High Card")
 
-# Deal a hand and print it
-hand = dealHand(testDeck)
-print("Hand: ", [str(card) for card in hand])
+def decide(min, pot):
+    decide = random.randint(min,20)
+    if decide > 10: 
+        val = botBank.giveTotal()
+        count = val /1.5
+        botBank.playChips(random.randint(1, count))
+        print("Bot bet: ", count)
+        pot = pot + count
+    if decide < 15:
+        val = playerBank.giveTotal()
+        pot = pot + val
+        botBank.allIn()
+        print("Bot went all in")
+    return pot
 
-# Test hand comparisons
-print("Is Flush? ", is_flush(hand))
-print("Is Straight? ", is_straight(hand))
-print("Is Straight Flush? ", is_straight_flush(hand))
-print("Is Royal Flush? ", is_royal_flush(hand))
-print("Is Three of a Kind? ", is_three_of_a_kind(hand))
-print("Is Four of a Kind? ", is_four_of_a_kind(hand))
 
+def playGame():
+    # Creates bot odds a value used to determine actions
+    BotOdds = 0
+    # Create a deck, shuffle it, and deal hands
+    testDeck = makeStandardDeck()
+    shuffleDeck(testDeck)
+
+    # Deal 2 cards to each player
+    hand1 = dealHand(testDeck, 2)
+    hand2 = dealHand(testDeck, 2)
+
+    # Deal 3 community cards
+    card_on_table = dealHand(testDeck, 3)
+
+    print("Hand 1: ", [str(card) for card in hand1])
+    #print("Hand 2: ", [str(card) for card in hand2])
+    print("Table Cards: ", [str(card) for card in card_on_table])
+
+    # Combine hands with community cards
+    combined_hand1 = hand1 + card_on_table
+    combined_hand2 = hand2 + card_on_table
+
+    # Evaluate hand values
+    value1 = hand_value(combined_hand1)
+    value2 = hand_value(combined_hand2)
+
+    print("Hand 1 Value: ", value1)
+    #print("Hand 2 Value: ", value2)
+    BotOdds = value2[0] + BotOdds
+    print("Bot Odds: ", BotOdds)
+
+    BetCheck = input("Player Would you like to bet or fold?(bet/fold): ")
+    if BetCheck == "fold":
+        print("Player Folded")
+        print("Nothing happened")
+        return
+    elif BetCheck == "bet":
+        print("Player Bets")
+        BotOdds = BotOdds - 1
+    else:
+        print("Invalid Input")
+        return
+    BetValue = input("How much would you like to bet?: (allin, or number)")
+    pot = 0
+    if BetValue == "allin":
+        val = playerBank.giveTotal()
+        pot = pot + val
+        playerBank.playChips(playerBank.allIn())
+        print("Player went all in")
+    else:
+        pot = pot + int(BetValue)
+        playerBank.playChips(int(BetValue))
+        print("Player bet: ", BetValue)
+    
+    pot = decide(BotOdds, pot)
+    print("Current Pot: ", pot)
+
+    
+    
+    print("Hand 1: ", [str(card) for card in hand1])
+    print("Hand 1 Value: ", value1)
+    print("Hand 2: ", [str(card) for card in hand2])
+
+    print("Hand 2 Value: ", value2)
+
+    # Determine the winner
+    if value1[0] > value2[0]:
+        print("Hand 1 wins!")
+        playerBank.add(pot)
+        playerBank.printTotal()
+        print("Bot then Player")
+        botBank.printTotal()
+        playerBank.printTotal()
+    elif value1[0] < value2[0]:
+        print("Hand 2 wins!")
+        botBank.add(pot)
+        print("Bot then Player")
+
+        botBank.printTotal()
+        playerBank.printTotal()
+    else:
+        print("It's a tie!")
+        playerBank.add(pot / 2)
+        botBank.add(pot / 2)
+        print("Bot then Player")
+
+        botBank.printTotal()
+        playerBank.printTotal()
+
+# Run the game
+playGame()
